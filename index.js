2a58f3b4384b14868331372c4b836c54a12d0849
shaniba firdouse <shanibafirdouse22@gmail.com>
2021-06-27 01:44:25 +0530

    incomplete action

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const request= require('request')
 const app=express()
 
 
-const apiKey=''
+const apiKey='c36572016d03391554f98725b9f3741f'
 
 app.use(express.static('public'))
 app.use(express.json());
@@ -17,7 +17,33 @@ app.use(express.urlencoded({
   app.set('view engine','ejs')
 
   app.get('/',function(req,res){
-      res.render('index',{wheather:null,error:null})
+      res.render('index',{weather:null,error:null})
+  })
+  app.post('/',(req,res)=>{
+      let city=req.body.city
+      let url=`api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+     // console.log(url)
+    // console.log(body);
+      request(url,function(err,response,body){
+        
+          if(err){
+              res.render('index',{weather:null,error:'Please try again'})
+          }
+          else{
+              let weather=JSON.parse(body)
+              
+              if(weather.main==undefined){
+                  res.render('index',{
+                      weather:null,
+                      error:'please try again'
+                  })
+              }else{
+                  weatherText=`It's ${weather.main.temp} degree celcius with ${weather.weather[0].main} in ${weather.name}`
+                  res.render('index',{weather:weatherText,error:null})
+                  console.log('body:',body);
+              }
+          }
+      })
   })
 
   app.listen(3000,function(){
